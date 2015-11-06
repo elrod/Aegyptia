b5b0b98a6510bb5095cf2fe770f3c388f0e66426
Jacopo Essenziale <j.essenziale@gmail.com>
2015-11-06 21:43:14 +0100

    First experiments: STONE TRAPS.
    1) Made a stone spawner, that can spawn both infinite or limited stones
    2) Stones have only a rigidbody and circle collider attached... we should probably write a stone behavior script, to get more fine control
    3) Stone trap can be activated by a trap switch...
    
    [TODO: we should formalize this trap thing by making a generic trap class with some common characteristics that all other traps should implement, just to keep the code a bit more reusable and organized]

---
+++ Assets/Script/Traps/TrapSwitch.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapSwitch : MonoBehaviour {
+
+	public GameObject relatedTrap;
+
+	void OnTriggerEnter2D(Collider2D col){
+		relatedTrap.SetActive(true);
+	}
+}
