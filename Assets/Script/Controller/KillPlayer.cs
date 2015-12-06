54128c531862f144d7c86e667805fe5f4189c598
Jacopo Essenziale <j.essenziale@gmail.com>
2015-12-06 18:45:15 +0100

    Edited KillPlayer script to fit the Poison case: if killAfterTime is set to true, the trap will kill after killTime, I set this to 4 seconds by default, so if the player stays in the poison for more than 4 seconds he dies!

--- Assets/Script/Controller/KillPlayer.cs
+++ Assets/Script/Controller/KillPlayer.cs
@@ -3,7 +3,12 @@ using System.Collections;
 
 public class KillPlayer : MonoBehaviour {
 
+    public bool killAfterTime = false;
+    public float killTime = 5f;
+
 	LevelManager levelManager;
+    bool inTrap;
+    float inTrapTime;
 
 	// Use this for initialization
 	void Start () {
@@ -17,8 +22,36 @@ public class KillPlayer : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D coll){
 		if (coll.tag == "Player") {
-			levelManager.RespawnPlayer();
-		}
+            if (killAfterTime)
+            {
+                /* This should happen only for poison right now... */
+                inTrap = true;
+                inTrapTime = Time.time;
+            }
+            else { 
+			    levelManager.RespawnPlayer();
+            }
+        }
 	}
+
+    void OnTriggerStay2D (Collider2D coll)
+    {
+        if(coll.tag == "Player" && inTrap)
+        {
+            if((Time.time - inTrapTime) >= killTime)
+            {
+                levelManager.RespawnPlayer();
+                inTrap = false;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.tag == "Player")
+        {
+            inTrap = false;
+        }
+    }
 
 }
