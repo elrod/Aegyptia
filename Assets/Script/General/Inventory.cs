0b92a0c3bdee5286ece10ffd039335e0a05a0c4f
Jacopo Essenziale <j.essenziale@gmail.com>
2015-12-10 16:48:16 +0100

    Basic inventory system, just code for now... need to work on an interface and a system to select objects

---
+++ Assets/Script/General/Inventory.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour {
+
+	public struct InventoryObject
+	{
+		public Tool tool;
+		public string name;
+		public bool autoConsume;
+	}
+
+	string selectedObject;
+
+	List<InventoryObject> inventory;	
+	// Use this for initialization
+	void Start () {
+		inventory = new List<InventoryObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// TODO: Complete by showing up the inventory and letting user select an object
+		if(Input.GetKeyDown(KeyCode.F)){
+			Use (selectedObject);
+		}
+	}
+
+	public void PickUp(GameObject theObject){
+		InventoryObject inventObj;
+		inventObj.tool = theObject.GetComponent<Tool>();
+		inventObj.name = theObject.name;
+		inventObj.autoConsume = true;
+		inventory.Add(inventObj);
+		selectedObject = inventObj.name;
+		Debug.Log (inventObj.name + " in inventory");
+	}
+
+	public void Use(string name){
+		foreach(InventoryObject obj in inventory){
+			if(obj.name.Equals(name)){
+				obj.tool.Use();
+				inventory.Remove(obj);
+				break;
+			}
+		}
+	}
+}
