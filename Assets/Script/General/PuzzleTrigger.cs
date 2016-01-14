5f6f1d64db6a7d65a2fbfb6e483074561fb40311
Jacopo Essenziale <j.essenziale@gmail.com>
2016-01-14 22:37:51 +0100

    I put a door behind the sphinx and made it move up when the puzzle is solved

--- Assets/Script/General/PuzzleTrigger.cs
+++ Assets/Script/General/PuzzleTrigger.cs
@@ -6,17 +6,24 @@ public class PuzzleTrigger : MonoBehaviour {
     public GameObject thePuzzle;
     public GameObject neededObject;
 
+	bool canMove = false;
+	public float deltaY = 3f;
+	Vector3 startPosition;
     string neededObjName = "";
 
 	// Use this for initialization
 	void Start () {
+		startPosition = transform.position;
         if(neededObject != null)
             neededObjName = neededObject.name;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		TabletPuzzle puzzle = thePuzzle.GetComponentInChildren<TabletPuzzle>();
+		if(canMove && transform.position.y < startPosition.y + deltaY){
+			transform.Translate(Vector3.up * Time.deltaTime);
+		}
 	}
 
     void OnTriggerEnter2D(Collider2D col)
@@ -33,4 +40,8 @@ public class PuzzleTrigger : MonoBehaviour {
             }
         }
     }
+
+	public void SetCanMove(bool value){
+		canMove = value;
+	}
 }
