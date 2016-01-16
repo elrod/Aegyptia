d592672f8d2f5a71b14be7989137e60a2c172938
Jacopo Essenziale <j.essenziale@gmail.com>
2016-01-16 20:21:10 +0100

    Column Generator implemented... still need to use it in real levels. First we need to put top decorations

---
+++ Assets/Script/ColumnGenerator/Editor/ColumnGenEditor.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(ColumnGen))]
+public class ColumnGenEditor : Editor
+{
+
+    ColumnGen generator;
+
+    public void OnEnable()
+    {
+        generator = (ColumnGen)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.Box("", new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(1) });
+        DrawDefaultInspector();
+        GUILayout.Box("", new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.Height(1) });
+        if (GUILayout.Button("Build Column"))
+        {
+            generator.GenerateColumn();
+        }
+    }
+}
