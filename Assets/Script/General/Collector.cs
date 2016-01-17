fa7f476c992d1c235553bf69a87764db80a9a40d
Jacopo Essenziale <j.essenziale@gmail.com>
2016-01-17 14:18:40 +0100

    Collectables in DemoLevel

--- Assets/Script/General/Collector.cs
+++ Assets/Script/General/Collector.cs
@@ -1,7 +1,11 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Collector : MonoBehaviour {
+
+    public Text collectedText;
+    public Text totalText;
 
     int toCollect;
     int collected;
@@ -10,11 +14,21 @@ public class Collector : MonoBehaviour {
 	void Start () {
         toCollect = FindObjectsOfType<Collectable>().Length;
         collected = 0;
-	}
+        totalText.text = toCollect.ToString();
+        if(collectedText == null)
+        {
+            Debug.LogError("COLLECTED TEXT NOT SET IN collector.cs, SET IT FROM INSPECTOR");
+        }
+        if (totalText == null)
+        {
+            Debug.LogError("COLLECTED TEXT NOT SET IN collector.cs, SET IT FROM INSPECTOR");
+        }
+    }
 	
 	public void ObjectCollected()
     {
         collected++;
+        collectedText.text = collected.ToString();
     }
 
     public int getCollected()
