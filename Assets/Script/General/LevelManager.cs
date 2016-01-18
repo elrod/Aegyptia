b7492c584d96ea0f7d83861e8131d91330d94c72
Jacopo Essenziale <j.essenziale@gmail.com>
2016-01-18 22:42:31 +0100

    Minor changes to final level...
    Keep pressed 1,2 or 3 for three seconds to jump to corrisponding level

--- Assets/Script/General/LevelManager.cs
+++ Assets/Script/General/LevelManager.cs
@@ -23,9 +23,14 @@ public class LevelManager : MonoBehaviour {
 	GameObject player;
 	PlayerGovernor playerGovernor;
 
-	
-	// Update is called once per frame
-	void Update () {
+    // Level Manual Selection
+    float timeToSelect = 3f;
+    float selectingStartTime = 0f;
+    bool selectingLevel = false;
+    int selectedLevel = 0;
+
+    // Update is called once per frame
+    void Update () {
 
 		// Find the active player
 		player = GameObject.FindGameObjectWithTag ("Player");
@@ -33,6 +38,36 @@ public class LevelManager : MonoBehaviour {
         if(endLevelPanel == null)
         {
             Debug.LogError("Please drag endLevelPanel from GUI to inspector in LevelManager");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectingLevel = true;
+            selectedLevel = 1;
+            selectingStartTime = Time.time;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectingLevel = true;
+            selectedLevel = 2;
+            selectingStartTime = Time.time;
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectingLevel = true;
+            selectedLevel = 3;
+            selectingStartTime = Time.time;
+        }
+        if (Input.GetKeyUp(KeyCode.Alpha1) || Input.GetKeyUp(KeyCode.Alpha2) || Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            selectingLevel = false;
+            selectedLevel = 0;
+            selectingStartTime = 0f;
+        }
+
+        if (selectingLevel && (Time.time - selectingStartTime) >= timeToSelect)
+        {
+            ManuallyLoadLevel(selectedLevel);
         }
 
 	}
@@ -99,5 +134,24 @@ public class LevelManager : MonoBehaviour {
             endLevelPanel.SetActive(true);
         }
         
+    }
+
+    // TRICKS AREA:
+
+    // Keep pressed the number of the level for 3 seconds to load it:
+    void ManuallyLoadLevel(int selectedLevel)
+    {
+        switch (selectedLevel)
+        {
+            case 1:
+                Application.LoadLevel("Tutorial");
+                break;
+            case 2:
+                Application.LoadLevel("DemoLevel");
+                break;
+            case 3:
+                Application.LoadLevel("FinalLevel");
+                break;
+        }
     }
 }
